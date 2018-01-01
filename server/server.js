0671277d2c10cb1eb7c51e349c4658de08d618f7
Nir Beck <nir@beckermus.com>
2018-01-01 19:22:57 +0200

    added GET/todos

--- server/server.js
+++ server/server.js
@@ -14,11 +14,27 @@ app.post('/todos',(req,res)=>{
     text:req.body.text
   });
   todo.save().then((doc)=>{
+
+  // var data = {
+  //   doc:doc,
+  //   message:'my message is here yay'
+  // }
     res.send(doc);
   },(err)=>{
     res.status(400).send(err);
   });
 });
+
+
+
+app.get('/todos',(req,res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  });
+},(e)=>{
+  res.status(400).send(e);
+});
+
 
 app.listen(3000,()=>{
   console.log('Started on port 3000');
